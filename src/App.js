b10a4b8c42def75b14b973a92a1a6a51ceb2af23
Thirdy Fetalvero <botsot.felix@gmail.com>
2021-07-01 00:16:37 +0800

    Added Row and API request

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Row from './Row';
+import requests from './request';
 
 function App() {
   return (
     <div className="App">
       <h1>Hey clever programmer lets build netflix clone</h1>
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now" />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}  />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
